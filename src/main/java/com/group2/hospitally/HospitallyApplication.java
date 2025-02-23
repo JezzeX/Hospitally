2138e8db0fc684ae9538de2d99a41d318b9bdc6b
JezzeX <jezzejagz4@gmail.com>
2025-02-23 19:39:08 +0100

    First major commit

---
+++ src/main/java/com/group2/hospitally/HospitallyApplication.java
@@ -0,0 +1,13 @@
+package com.group2.hospitally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitallyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitallyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group2/hospitally/HospitallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group2.hospitally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitallyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
